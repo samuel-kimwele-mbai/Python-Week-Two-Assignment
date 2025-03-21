1834688dee222334baaa6808065fab40df9b27c9
samuel-kimwele-mbai <mbaisamuel127@gmail.com>
2025-03-21 11:59:04 +0300

    Answers to week 2Python  Assignmnmet

---
+++ week2-assignment.py
@@ -0,0 +1,8 @@
+my_list = [10, 20, 30, 40]  
+my_list.insert(1, 15)       
+my_list += [50, 60, 70]     
+del my_list[-1]             
+my_list.sort()              
+print("Index of 30:", my_list.index(30))
+
+print("Final List:", my_list) 
